6b0aaf521b3d7f18d25fb61110bf6a3898d8e00d
Adam McHugh <adam@mchughsecurity.com>
2022-05-13 20:21:02 +0930

    Update the run script so it binds the ./storage directory to /storage

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sudo docker run -it --rm --name ail-feeder-telegram --env-file=.env --network=host ail-feeder-telegram
+sudo docker run -it --rm --name ail-feeder-telegram --env-file=.env --network=host --mount type=bind,src=`pwd`/storage,dst=/storage ail-feeder-telegram
